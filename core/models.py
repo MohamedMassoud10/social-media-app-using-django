889a5322afe4305eac9f37c25058bae63eb62f13
MohamedMassoud10 <massoudfcai@gmail.com>
2024-03-25 01:38:54 +0200

    finished the Profile Model

--- core/models.py
+++ core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+User =get_user_model()
+
 class Profile(models.Model):
-    username=pass
-    id_user=pass
-    bio=pass
-    profileimage=models.ImageField()
-    location=pass
+    username=models.ForeignKey(User,on_delete=models.CASCADE)
+    id_user=models.IntegerField()
+    bio=models.TextField(blank=True)
+    profileimage=models.ImageField(upload_to='profile_images/%y/%m/%d',default='blank_profile_pic.png')
+    location=models.CharField(max_length=50,blank=True)
 
