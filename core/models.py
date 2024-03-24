5259c3b14b5b4c7d4c433d3356d8e8617958486a
MohamedMassoud10 <massoudfcai@gmail.com>
2024-03-24 23:42:39 +0200

    added profile model and handel the media folder

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    username=pass
+    id_user=pass
+    bio=pass
+    profileimage=models.ImageField()
+    location=pass
+
