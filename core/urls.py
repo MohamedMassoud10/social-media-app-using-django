60fc56efec2cb7085020688f76773b2f1e516da5
MohamedMassoud10 <massoudfcai@gmail.com>
2024-04-04 21:37:48 +0200

    fixed the migration problem

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('signup',views.signup,name='signup'),
     path('signin/', views.signin, name='signin'),
-    path('settings',views.setting,name='setting'),
+    path('settings',views.setting,name='settings'),
     path('logout',views.logout,name='logout'),
 ]
