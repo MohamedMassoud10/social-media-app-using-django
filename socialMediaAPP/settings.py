8803e3925dbda7455a44f68a711303f554e381f5
MohamedMassoud10 <massoudfcai@gmail.com>
2024-04-05 15:15:01 +0200

    added netlift dmoan

--- socialMediaAPP/settings.py
+++ socialMediaAPP/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+k6=hf!km56gm@wg8)zdd=p1++b1*!5s*c2^zzo%)&ji*&l=og
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1','.onrender.com']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1','.onrender.com','.netlify.com']
 
 
 # Application definition
